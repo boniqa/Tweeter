f2b53c90f74c8eaaeb19085797ca90deadfbf2f7
Weronika <wera@student.edu>
2017-03-28 09:37:15 +0200

    change log

--- main.php
+++ main.php
@@ -1,11 +1,8 @@
 <?php
 
-define('DB_HOST' , "localhost");
-define('DB_USER' , "root");
-define ('DB_PASSWORD' , "coderslab");
-define('DB_DBNAME' , "tweet_db");
+include 'config.php';
 
-$conn= new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DBNAME);
+$conn= new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_DBNAME);
 
     if ($conn->connect_error){
         die("POłączenie nieudane, błąd".$conn->connect_error);
@@ -13,3 +10,32 @@ $conn= new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DBNAME);
     else{
         echo 'połaczenie działa';
     }
+    // alt+ insert -> getery + setery ;
+    /*
+     Create table Users (
+     id int NOT NULL AUTO_INCREMENT,
+     email varchar(255) NOT NULL UNIQUE,
+     user_name varchar(255) NOT NULL,
+     hashed_password varchar(60) NOT NULL,
+     PRIMARY KEY(id)  
+     );
+
+     */
+     /*
+      * 3 pliki: klasa user, main, config.php + klasa tweet, 
+      *  ignore net beans do gita; oddzielny plik;
+      */
+    
+    
+    /*
+     * Create table tweets (
+        id int NOT NULL AUTO_INCREMENT,
+        user_id int NOT NULL,
+        text varchar(160) NOT NULL,
+        creation_date DATETIME NOT NULL,
+        PRIMARY KEY(id)  ,
+     * foreign key (user_id)
+     * REFERENCES Users(id) ON DELETE CASCADE
+     );
+     */
+    
