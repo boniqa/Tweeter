1f7f884603705a81b09b9572ad4e780522da4dad
boniqa <budnik.wer@gmail.com>
2017-09-06 23:37:47 +0200

    TEST

--- my_messages.php
+++ my_messages.php
@@ -10,33 +10,34 @@ if(!isset($_SESSION['loggedUserId'])) {
 	header("Location: login.php");
 }
 
-        
+
+				
         $user_id= $_SESSION['loggedUserId'];
         $usr= new User();
-        $res= $usr->loadUserById($conn, $user_id);     
+        $res= $usr->loadUserById($conn, $user_id);
 
         if($_SERVER['REQUEST_METHOD'] == 'POST') {
     if(isset($_POST['tweet'])){
-        
+
       $tweet_text = ($_POST['tweet']);
       $user_id= $_SESSION['loggedUserId'] ;
-      
+
       $new_tweet= new Tweet();
-                
+
         $new_tweet->setText($tweet_text);
         $new_tweet->setUserId($user_id);
         $new_tweet->setCreationDate(date("Y-m-d H:i:s"));
         $new_tweet->saveToDb($conn);
-        
-       // echo"new tweet is set!";              
-             
+
+       // echo"new tweet is set!";
+
       //var_dump($user_id);
       //var_dump($tweet_text);
     }
-    
+
 }
-                 
- 
+
+
 
 ?>
 
@@ -48,25 +49,25 @@ if(!isset($_SESSION['loggedUserId'])) {
     <title>Twttr </title>
     <link href="style/style.css" rel="stylesheet">
     <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
-      integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+      integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
       crossorigin="anonymous">
 <!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" 
-      integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+      integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp"
       crossorigin="anonymous">
 <!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" 
-integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" 
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
 crossorigin="anonymous"></script>
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"/>
 </head>
-<body>    
+<body>
 <nav class="navbar navbar-default navbar-fixed-top">
       <div class="container">
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
-              <li class="active"><a href="main.php">Home</a></li>                
+              <li class="active"><a href="main.php">Home</a></li>
           </ul>
           <ul class="nav navbar-nav navbar-right">
               <li><a href="my_messages.php"> My messages</a></li>
@@ -77,15 +78,15 @@ crossorigin="anonymous"></script>
       </div>
     </nav>
     <div class="jumbotron"><h2 style="margin-left: 100px">Profile user: <?php echo $res->getUsername()?></h2></div>
-    
-    
-    
-    
+
+
+
+
     <div class="page-header">
         <h2 style="margin-left: 100px">Users Tweets:</h2>
     </div>
-    
-    
+
+
     <table class="table table-striped">
             <thead>
               <tr>
@@ -100,16 +101,16 @@ crossorigin="anonymous"></script>
                 <?php
                  $tweets= new Tweet();
                 $result= $tweets->loadTweetByUserId($conn, $user_id);
-                               
-             
+
+
                 foreach ($result as $row){
-                    
+
                 $comments= new Comment();
-                $res= $comments->loadAllCommentsByPostId($conn, $row->getId());                
+                $res= $comments->loadAllCommentsByPostId($conn, $row->getId());
                 //var_dump($res);
                 //echo count($res);
-                
-                
+
+
                     echo "<tr>
                 <td>".$row->getId()."</td>
                 <td>".$row->getText(). "</td>
@@ -120,13 +121,13 @@ crossorigin="anonymous"></script>
                 <br>
 
                 </tr>"
-                ;}  
-                
-               
+                ;}
+
+
                ?>
             </tbody>
           </table>
-    
+
     <div class="input-group">
         <form method="POST" action="#">
         <input type="text" maxlength="120"  name ="tweet" class="form-control" placeholder="Send message" aria-describedby="sizing-addon2">
